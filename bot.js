320b86184f5b72b524d69f73c85ce79568934a5f
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-06-05 20:17:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,7 +172,7 @@ if (msg.content.toLocaleLowerCase() === prefix + "evet") {
   return msg.channel.send(embed)
 }
  if (msg.content.toLowerCase() === prefix + "yayın") {
-   msg.reply('şu anda yayında değil.Lütfen daha sonra yaz.)
+   msg.reply('şu anda yayında değil.Lütfen daha sonra yaz.')
  }
 });
 
