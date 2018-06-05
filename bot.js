53b159d3807ff5580395b392279a05e542a02cf8
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-06-05 20:17:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,7 +172,7 @@ if (msg.content.toLocaleLowerCase() === prefix + "evet") {
   return msg.channel.send(embed)
 }
  if (msg.content.toLowerCase() === prefix + "yayın") {
-   msg.reply('https://www.youtube.com/watch?v=MvQvj-WJ1iQ')
+   msg.reply('şu anda yayında değil.Lütfen daha sonra yaz.)
  }
 });
 
