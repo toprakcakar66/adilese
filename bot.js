78108eac01a8d888c7783e5073a907f547d7d571
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-07-01 21:44:18 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,13 +31,13 @@ client.on('message', msg => {
     msg.reply('Aleyküm selam!');
   }
     if (msg.content.toLowerCase() === prefix + 'yayın') {
-    msg.channel.sendMessage('@everyone https://www.youtube.com/watch?v=_NauPLFsDhI&feature=youtu.be');
+    msg.channel.sendMessage('@everyone https://www.youtube.com/watch?v=BIN-yfMcANU');
   }
     if (msg.content.toLowerCase() === prefix + 'kurallar') {
     msg.channel.sendMessage('Discord Kuralları @everyone \nSpam Kesinlikle YASAKTIR```1-Profilinizde isim ve yaş zorunludur ve nickinizin başında Sembol YASAKTIR.\n2-Chatte ve Konuşma Kanallarında her türlü spam,troll ve küfür yasaktır.\n3-Chatten veya özelden item istemek (bana item atabilirmisiniz) yasaktır.4-Küfür, Link, Irkçılık, Din, Spam, Flood, Siyaset ve 3.Şahıs Hakkında Konuşmak Kesinlikle Yasaktır!```Bunlara uymayanlar ilk önce uyarılır. Uyarıyı dikkate almayanlar veya kuralları çiğneyenler\nSüresiz olarak BANLANIR.');
   }  
     if (msg.content.toLowerCase() === prefix + 'yayin') {
-    msg.channel.sendMessage('@everyone https://www.youtube.com/watch?v=_NauPLFsDhI&feature=youtu.be');
+    msg.channel.sendMessage('@everyone https://www.youtube.com/watch?v=BIN-yfMcANU');
   }
     if (msg.content.toLowerCase() === prefix + '155') {
     msg.reply('Bir Olaymi Oldu? Polisle Konusun ```@Discord Mod\n@Moderasyon Ekibi \n@YONETICI \n@Dc Mod Lideri```');
